71a077f571c158b6d87e952210adfd2b6d3de209
py <hzpengyang@corp.netease.com>
2012-12-28 17:57:46 +0800

    delete getUser method

--- game-server/app/servers/chat/handler/chatHandler.js
+++ game-server/app/servers/chat/handler/chatHandler.js
@@ -47,19 +47,3 @@ handler.send = function(msg, session, next) {
 		route: msg.route
 	});
 };
-
-/**
- * Get online users
- *
- * @param {Object} msg message from client
- * @param {Object} session
- * @param {Function} next next stemp callback
- *
- */
-handler.getUsers = function(msg, session, next) {
-	var remote = chatRemote(this.app);
-	var users = remote.get(msg.rid, false);
-	next(null, {
-		users: users
-	});
-};
